d9e15b7f2f93e178e6f71b867971a753fa0d2bdd
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-05 17:41:55 -0700

    Added class inheritance

--- class/app.js
+++ class/app.js
@@ -1,3 +1,14 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/** CLASSES */
 var Person = (function () {
     function Person(name, userName, age) {
         this._type = 'DEFAULT';
@@ -10,14 +21,28 @@ var Person = (function () {
     };
     Person.prototype.setType = function (type) {
         this._type = type;
-        console.log(this._type);
     };
     Person.prototype.getType = function () {
         return this._type;
     };
     return Person;
 }());
-var person = new Person('Emerald', 'eslaurente', 28);
+var person = new Person('Max', 'max1234', 24);
 person.setType('Administrator');
 console.log(person);
 console.log("Name: " + person.name + " | User Name: " + person.userName + " | Age: " + person.getAge() + " | Type: " + person.getType());
+/** INHERITANCE */
+var Emerald = (function (_super) {
+    __extends(Emerald, _super);
+    function Emerald() {
+        var _this = _super.call(this, '', '', 0) || this;
+        _this.name = 'Emerald';
+        _this.age = 28;
+        _this.userName = 'eslaurente';
+        _this.setType('Normal');
+        return _this;
+    }
+    return Emerald;
+}(Person));
+var emerald = new Emerald();
+console.log("Name: " + emerald.name + " | User Name: " + emerald.userName + " | Age: " + emerald.getAge() + " | Type: " + emerald.getType());
--- class/app.ts
+++ class/app.ts
@@ -1,3 +1,4 @@
+/** CLASSES */
 class Person {
     name: string;
     userName: string;
@@ -16,7 +17,6 @@ class Person {
 
     setType(type: string) {
         this._type = type;
-        console.log(this._type);
     }
 
     getType() {
@@ -24,8 +24,21 @@ class Person {
     }
 }
 
-const person = new Person('Emerald', 'eslaurente', 28);
+const person = new Person('Max', 'max1234', 24);
 person.setType('Administrator');
 console.log(person);
 console.log(`Name: ${person.name} | User Name: ${person.userName} | Age: ${person.getAge()} | Type: ${person.getType()}`);
+
+/** INHERITANCE */
+class Emerald extends Person {
+    constructor() {
+        super('', '', 0);
+        this.name = 'Emerald';
+        this.age = 28;
+        this.userName = 'eslaurente';
+        this.setType('Normal');
+    }
+}
+const emerald = new Emerald();
+console.log(`Name: ${emerald.name} | User Name: ${emerald.userName} | Age: ${emerald.getAge()} | Type: ${emerald.getType()}`);
 
