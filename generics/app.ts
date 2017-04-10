a7328efa53c08117bff7a97d6ce65ba80c93a38d
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-09 18:37:22 -0700

    Added Arrays generics

--- generics/app.ts
+++ generics/app.ts
@@ -1,10 +1,10 @@
-
 /** SIMPLE GENERIC */
 function echo(data: any) {
     return data;
 }
 console.log(echo('Emerald ' + 28));
 console.log(echo({ name: 'Emerald', age: 28}));
+
 
 /** BETTER GENERIC */
 function betterEcho<T>(data: T): T {
@@ -15,3 +15,17 @@ console.log(betterEcho('A String Value').toUpperCase());
 console.log(betterEcho({ someString: 'Some String', someNumber: 99 }).someString.toUpperCase());
 console.log(betterEcho<Number>(1024.0014).toFixed(0));
 
+
+/** BUILT-IN GENERICS - Array Generic */
+const testResults: Array<Number> = [1.19, 1024, 3479];
+// testResults.push('4347'); // ERROR
+testResults.push(0);
+
+
+/** ARRAYS GENERICS */
+function printAll<T>(args: T[]): void {
+    args.forEach((element) => console.log(element));
+}
+// printAll('NOT AN ARRAY'); // ERROR
+// printAll<Array<String>([34, 79, 97, 99]) // ERROR
+printAll<String>(['34', '79', '97', '99']);
