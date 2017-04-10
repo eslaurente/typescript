a553521a9d0ea0676cb0c3e6ba94166667b94af1
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-09 18:24:47 -0700

    Generic function

--- generics/app.ts
+++ generics/app.ts
@@ -1,1 +1,17 @@
 
+/** SIMPLE GENERIC */
+function echo(data: any) {
+    return data;
+}
+console.log(echo('Emerald ' + 28));
+console.log(echo({ name: 'Emerald', age: 28}));
+
+/** BETTER GENERIC */
+function betterEcho<T>(data: T): T {
+    return data;
+}
+console.log(betterEcho('A String Value').toUpperCase());
+// console.log(betterEcho('1234').toFixed(3); // ERROR
+console.log(betterEcho({ someString: 'Some String', someNumber: 99 }).someString.toUpperCase());
+console.log(betterEcho<Number>(1024.0014).toFixed(0));
+
