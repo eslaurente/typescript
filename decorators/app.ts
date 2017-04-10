fea261dd315e5d1cb466b92d3de7f43768aac7c7
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-09 22:15:03 -0700

    Multiple decorators

--- decorators/app.ts
+++ decorators/app.ts
@@ -20,7 +20,7 @@ class Car {
 }
 
 
-/** ADVANCED  */
+/** ADVANCED DECORATOR */
 function printable(constructorFn: Function) {
     constructorFn.prototype.print = function() {
         console.log('PRINT method from: ', this);
@@ -28,6 +28,7 @@ function printable(constructorFn: Function) {
 }
 
 @printable
+@logging(true)  // ** MULTIPLE DECORATORS **
 class Plant {
     name = 'GREEN PLANT';
 }
@@ -35,3 +36,17 @@ const plant = new Plant();
 if ((plant as any).print instanceof Function) {
     (plant as any).print();
 }
+
+// ** OUTPUT **
+// function Person(name) {
+//         if (name === void 0) { name = 'PERSON'; }
+//         this.name = name;
+//     }
+// app.js:9 function Car(name) {
+//         if (name === void 0) { name = 'CAR'; }
+//         this.name = name;
+//     }
+// app.js:9 function Plant() {
+//         this.name = 'GREEN PLANT';
+//     }
+// app.js:38 PRINT method from:  Plant {name: "GREEN PLANT"}
